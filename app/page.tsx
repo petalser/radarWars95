a94d4d999457e23cc07d820fb736aa793ab7251e
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-01-03 16:44:42 +0100

    Refactored DesktopIcon to accept children

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import DesktopIcon from "@/components/DesktopIcon";
-import type { IconType } from "@/components/DesktopIcon";
+import { Desk100 } from "@react95/icons";
+import { IconRadar } from "@/components/icons/IconRadar";
 
 const desktopIcons = [
   {
     text: "Desktop",
-    icon: "Desk100"
+    icon: Desk100
   },
   {
     text: "Radar Wars",
-    icon: "Wab321014"
+    icon: IconRadar
   },
 ]
 
@@ -16,7 +17,9 @@ export default function Home() {
   return (
     <>
       {desktopIcons.map((item, index) => {
-        return <DesktopIcon text={item.text} icon={item.icon as IconType} key={index} />
+        return <DesktopIcon text={item.text} key={index} >
+          <item.icon className="mx-auto mt-1" />
+        </DesktopIcon>
       })}
     </>
   )
