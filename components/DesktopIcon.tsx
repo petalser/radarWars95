73dfaa8fd1bb2bc2b07f3b7f66f1ba036c169c76
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-01-12 15:10:12 +0100

    Turned  div into link

--- components/DesktopIcon.tsx
+++ components/DesktopIcon.tsx
@@ -1,17 +1,18 @@
 import { Cursor } from "@react95/core";
-
+import Link from "next/link";
 
 type DesktopIconProps = {
     text: string;
+    url?: string;
     children: React.ReactNode;
 };
 
-export default function DesktopIcon({ text, children, variant = '32x32_4' }: DesktopIconProps) {
+export default function DesktopIcon({ text, children, url = "/404" }: DesktopIconProps) {
     return (
-        <div className={`size-16 ${Cursor.Pointer} border border-red-500 flex hover:bg-opacity-20 hover:bg-white flex-col truncate`}>
+        <Link href={url} className={`size-16 ${Cursor.Pointer} border border-red-500 flex hover:bg-opacity-20 hover:bg-white flex-col truncate`}>
             {children}
             <br />
             <p className="mx-auto my-0 p-0 text-white">{text}</p>
-        </div>
+        </Link>
     );
 }
