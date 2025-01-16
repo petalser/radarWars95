64d7cddb0f5823c93b743c409b2cbbabda12d0f2
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-01-16 19:01:00 +0100

    Added logging on socket config

--- config/socket.ts
+++ config/socket.ts
@@ -2,3 +2,9 @@ import { io } from "socket.io-client";
 const URL = "http://localhost:3000/"
 
 export const socket = io(URL);
+
+socket.on("connect", () => { console.log("Socket.config: connected") })
+
+socket.on("connect_error", (error) => {
+    console.log(`Socket.config: ${error}`);
+});
